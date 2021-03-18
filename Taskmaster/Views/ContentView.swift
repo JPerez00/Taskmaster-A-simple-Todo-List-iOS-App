07218acb3ef13657eb9e7465a4b84169680fab40
Jorge Perez <info@jperez.ca>
2021-03-18 17:02:54 -0600

    Initial commit from Xcode 12

---
+++ Taskmaster/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Taskmaster
+//
+//  Created by Jorge Perez on 2021-03-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Taskmaster!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
